f702ba64ed1ea8e39e629a94e938507422fd1606
Serhii Hrynyshyn <sergandd@gmail.com>
2023-09-14 22:26:37 +0300

    Body of Shortest step

--- main.py
+++ main.py
@@ -1,4 +1,32 @@
+def shortest_step(goal_num: int) -> int:
+    # write your code here
+    step_counter = 0
 
-print("Hello")
+    def divide(value):
+        counter = 0
+        print(value, value % 2)
+        while value % 2 == 0:
+            value = value / 2
+            counter += 1
+        return counter
 
-# test
+    def plus(value):
+        counter = 0
+        print(value, value % 2)
+        while value % 2 != 0:
+            value -= 1
+            counter += 1
+
+            print(">>", value, value % 2)
+        return counter
+
+    while goal_num > 1:
+        step_counter += divide(goal_num)
+        step_counter += plus(goal_num)
+
+
+
+    return step_counter
+
+
+print(shortest_step(1000))
